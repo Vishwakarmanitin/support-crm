3cd6be4e7ec17e95fa8425cf64f8a45c844bb5f7
Vishwakarmanitin <vishwakarmanitin209@gmail.com>
2026-07-22 18:34:51 +0530

    Update frontend to use Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://support-crm-production-00dc.up.railway.app",
 });
 
 export default api;
